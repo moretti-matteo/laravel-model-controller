65e8a6086e7c97b73cae468fdafe6d3429ad7650
moretti-matteo <matteomoretti2000@hotmail.it>
2022-07-11 17:44:27 +0200

    cambiata condizione if nel controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,11 +7,14 @@ use App\Movie;
 
 class MovieController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $movies = Movie::all();
-        
-        if(count($movies) > 0){
-            return view("welcome",compact('movies'));
-        }else return view("notfound");
+
+        if (count($movies) === 0) {
+            return view("notfound");
+        }
+
+        return view("welcome", compact('movies'));
     }
 }
